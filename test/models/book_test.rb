8b3ce17cfe61afc63d6d0abe162db49641e1e80e
Chris <clai88@gmail.com>
2016-07-26 20:02:01 -0400

    created author controller

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "can be persisted" do
+    pig = Book.create(title: "3 little pigs")
+    assert pig.persisted?
+  end
 end
