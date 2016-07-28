7dd38fb545d9186c1ab4fe60e259023cd2ada6a1
Chris <clai88@gmail.com>
2016-07-27 21:52:28 -0400

    linked pages together, added warning message if page is overdue

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,8 @@
 class ReservationsController < ApplicationController
   #get
   def book
+    @book = Book.find(params["id"])
+    @reservations = Reservation.all.select{|i| i.book_id == @book.id}
     render :book_reservation
   end
   #get
@@ -10,15 +12,26 @@ class ReservationsController < ApplicationController
 
     render :all_reservations
   end
+
   #post
   def add
     @reservation = Reservation.create(reservation_params)
+    @reservation.update(due_on: Time.now + 14.days)
     redirect_to "/reservations"
   end
+
   #get
   def details
-    #code
+    # @book = Book.find(params["id"])
+    # @reservations = Reservation.all.select{|i| i.book_id == @book.id}
+    # @current_reservation = @reservations[-1]
+
+    @reservation = Reservation.find(params["id"])
+    @book = @reservation.book
+
+    render :current_reservation
   end
+
   #patch
   def return
     #code
